79eeb71a392633c0f8f2bd1ef48826692234d21c
太阳雨 <zfz1015@outlook.com>
2019-06-14 11:12:16 +0800

    Update prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,6 +3,3 @@ cd language_model
 wget https://storage.googleapis.com/bert_models/2018_11_03/chinese_L-12_H-768_A-12.zip
 unzip chinese_L-12_H-768_A-12.zip
 
-#python package:
-pip3 install -r requirements.txt
-
