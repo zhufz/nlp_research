2f1be67064ce5d374e642e347c19fcfee1a98ef7
zfz <zfz@zfzdeMacBook-Pro.local>
2019-06-17 20:52:37 +0800

    -

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -6,14 +6,12 @@ from classify import Classify
 from classify_m import ClassifyM
 from match import Match
 from ner import NER
-from seq_generate import SeqGenerate
 from translation import Translation
 
 dl_tasks = {}
 dl_tasks['classify'] = Classify
 dl_tasks['match'] = Match
 dl_tasks['ner'] = NER
-dl_tasks['seq_generate'] = SeqGenerate
 dl_tasks['translation'] = Translation
 
 
