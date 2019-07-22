71024e6cf6d9f7f145ba7933fbf062072953c22e
zfz <zfz@zfzdeMacBook-Pro.local>
2019-07-22 20:26:39 +0800

    reference

--- language_model/bilm-tf/start.sh
+++ language_model/bilm-tf/start.sh
@@ -1,10 +1,11 @@
 #git :https://github.com/allenai/bilm-tf
+#other:https://github.com/nefujiangping/entity_recognition/blob/master/README.md#how-to-train-a-pure-token-level-elmo-from-scratch
 
 #setup:
-python setup.py install
+#python setup.py install
 
 # prepare vocab file
-python3 create_vocab.py  data/corpus.txt
+#python3 create_vocab.py  data/corpus.txt
 
 # train
 python3 bin/train_elmo.py \
